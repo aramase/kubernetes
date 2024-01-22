997a9b797a5d732c3932a7fea97f23fbb4ec94c3
Anish Ramasekar <anish.ramasekar@gmail.com>
2024-01-22 21:27:13 +0000

    wip00
    Signed-off-by: Anish Ramasekar <anish.ramasekar@gmail.com>

--- staging/src/k8s.io/apiserver/pkg/authentication/cel/interface.go
+++ staging/src/k8s.io/apiserver/pkg/authentication/cel/interface.go
@@ -18,12 +18,8 @@ limitations under the License.
 package cel
 
 import (
-	"context"
-
 	celgo "github.com/google/cel-go/cel"
 	"github.com/google/cel-go/common/types/ref"
-
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 // ExpressionAccessor is an interface that provides access to a CEL expression.
@@ -48,23 +44,6 @@ type EvaluationResult struct {
 type Compiler interface {
 	CompileClaimsExpression(expressionAccessor ExpressionAccessor) (CompilationResult, error)
 	CompileUserExpression(expressionAccessor ExpressionAccessor) (CompilationResult, error)
-}
-
-// ClaimsMapper provides a CEL expression mapper configured with the claims CEL variable.
-type ClaimsMapper interface {
-	// EvalClaimMapping evaluates the given claim mapping expression and returns a EvaluationResult.
-	// This is used for username, groups and uid claim mapping that contains a single expression.
-	EvalClaimMapping(ctx context.Context, claims *unstructured.Unstructured) (EvaluationResult, error)
-	// EvalClaimMappings evaluates the given expressions and returns a list of EvaluationResult.
-	// This is used for extra claim mapping and claim validation that contains a list of expressions.
-	EvalClaimMappings(ctx context.Context, claims *unstructured.Unstructured) ([]EvaluationResult, error)
-}
-
-// UserMapper provides a CEL expression mapper configured with the user CEL variable.
-type UserMapper interface {
-	// EvalUser evaluates the given user expressions and returns a list of EvaluationResult.
-	// This is used for user validation that contains a list of expressions.
-	EvalUser(ctx context.Context, userInfo *unstructured.Unstructured) ([]EvaluationResult, error)
 }
 
 var _ ExpressionAccessor = &ClaimMappingExpression{}
--- staging/src/k8s.io/apiserver/pkg/authentication/cel/mapper.go
+++ staging/src/k8s.io/apiserver/pkg/authentication/cel/mapper.go
@@ -20,11 +20,11 @@ import (
 	"context"
 	"fmt"
 
+	celgo "github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common/types/ref"
+
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
-
-var _ ClaimsMapper = &mapper{}
-var _ UserMapper = &mapper{}
 
 // mapper implements the ClaimsMapper and UserMapper interface.
 type mapper struct {
@@ -34,12 +34,12 @@ type mapper struct {
 // CELMapper is a struct that holds the compiled expressions for
 // username, groups, uid, extra, claimValidation and userValidation
 type CELMapper struct {
-	Username             ClaimsMapper
-	Groups               ClaimsMapper
-	UID                  ClaimsMapper
-	Extra                ClaimsMapper
-	ClaimValidationRules ClaimsMapper
-	UserValidationRules  UserMapper
+	Username             StringMapper
+	Groups               StringArrayMapper
+	UID                  StringMapper
+	Extra                MapStringStringArrayMapper
+	ClaimValidationRules BoolMapper
+	UserValidationRules  BoolMapper
 }
 
 // NewClaimsMapper returns a new ClaimsMapper.
@@ -94,4 +94,174 @@ func (m *mapper) eval(ctx context.Context, input map[string]interface{}) ([]Eval
 	}
 
 	return evaluations, nil
+}
+
+func eval(ctx context.Context, input map[string]interface{}, compilationResults ...CompilationResult) ([]EvaluationResult, error) {
+	evaluations := make([]EvaluationResult, len(compilationResults))
+
+	for i, compilationResult := range compilationResults {
+		var evaluation = &evaluations[i]
+		evaluation.ExpressionAccessor = compilationResult.ExpressionAccessor
+
+		evalResult, _, err := compilationResult.Program.ContextEval(ctx, input)
+		if err != nil {
+			return nil, fmt.Errorf("expression '%s' resulted in error: %w", compilationResult.ExpressionAccessor.GetExpression(), err)
+		}
+
+		evaluation.EvalResult = evalResult
+	}
+
+	return evaluations, nil
+}
+
+type BoolMapper interface {
+	EvalBool(ctx context.Context, claims *unstructured.Unstructured) (bool, error)
+}
+
+type StringMapper interface {
+	EvalString(ctx context.Context, claims *unstructured.Unstructured) (string, error)
+}
+
+type StringArrayMapper interface {
+	EvalStringArray(ctx context.Context, claims *unstructured.Unstructured) ([]string, error)
+}
+
+type MapStringStringArrayMapper interface {
+	EvalMapStringStringArray(ctx context.Context, claims *unstructured.Unstructured) (map[string][]string, error)
+}
+
+type usernameMapper struct {
+	compilationResult CompilationResult
+}
+
+func (m *usernameMapper) EvalString(ctx context.Context, claims *unstructured.Unstructured) (string, error) {
+	evaluations, err := eval(ctx, claims.Object, m.compilationResult)
+	if err != nil {
+		return "", err
+	}
+	if len(evaluations) != 1 {
+		return "", fmt.Errorf("expected 1 evaluation result, got %d", len(evaluations))
+	}
+	if evaluations[0].EvalResult.Type() != celgo.StringType {
+		return "", fmt.Errorf("expected string return type, got %s", evaluations[0].EvalResult.Type())
+	}
+	return evaluations[0].EvalResult.Value().(string), nil
+}
+
+type groupsMapper struct {
+	compilationResult CompilationResult
+}
+
+func (m *groupsMapper) EvalStringArray(ctx context.Context, claims *unstructured.Unstructured) ([]string, error) {
+	evaluations, err := eval(ctx, claims.Object, m.compilationResult)
+	if err != nil {
+		return nil, err
+	}
+	if len(evaluations) != 1 {
+		return nil, fmt.Errorf("expected 1 evaluation result, got %d", len(evaluations))
+	}
+
+	return convertCELValueToStringList(evaluations[0].EvalResult)
+}
+
+type uidMapper struct {
+	compilationResult CompilationResult
+}
+
+func (m *uidMapper) EvalString(ctx context.Context, claims *unstructured.Unstructured) (string, error) {
+	evaluations, err := eval(ctx, claims.Object, m.compilationResult)
+	if err != nil {
+		return "", err
+	}
+	if len(evaluations) != 1 {
+		return "", fmt.Errorf("expected 1 evaluation result, got %d", len(evaluations))
+	}
+	if evaluations[0].EvalResult.Type() != celgo.StringType {
+		return "", fmt.Errorf("expected string return type, got %s", evaluations[0].EvalResult.Type())
+	}
+	return evaluations[0].EvalResult.Value().(string), nil
+}
+
+type extraMapper struct {
+	compilationResult CompilationResult
+}
+
+func (m *extraMapper) EvalMapStringStringArray(ctx context.Context, claims *unstructured.Unstructured) (map[string][]string, error) {
+	evaluations, err := eval(ctx, claims.Object, m.compilationResult)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string][]string, len(evaluations))
+	for _, evaluation := range evaluations {
+		extraMapping, ok := result.ExpressionAccessor.(*authenticationcel.ExtraMappingExpression)
+		if !ok {
+			return nil, fmt.Errorf("oidc: error evaluating extra claim expression: %w", fmt.Errorf("invalid type conversion, expected ExtraMappingCondition"))
+		}
+
+		extraValues, err := convertCELValueToStringList(result.EvalResult)
+		if err != nil {
+			return nil, fmt.Errorf("oidc: error evaluating extra claim expression: %s: %w", extraMapping.Expression, err)
+		}
+
+		if len(extraValues) == 0 {
+			continue
+		}
+
+		extra[extraMapping.Key] = extraValues
+	}
+}
+
+// convertCELValueToStringList converts the CEL value to a string list.
+// The CEL value needs to be either a string or a list of strings.
+// "", [] are treated as not being present and will return nil.
+// Empty string in a list of strings is treated as not being present and will be filtered out.
+func convertCELValueToStringList(val ref.Val) ([]string, error) {
+	switch val.Type().TypeName() {
+	case celgo.StringType.TypeName():
+		out := val.Value().(string)
+		if len(out) == 0 {
+			return nil, nil
+		}
+		return []string{out}, nil
+
+	case celgo.ListType(nil).TypeName():
+		var result []string
+		switch val.Value().(type) {
+		case []interface{}:
+			for _, v := range val.Value().([]interface{}) {
+				out, ok := v.(string)
+				if !ok {
+					return nil, fmt.Errorf("expression must return a string or a list of strings")
+				}
+				if len(out) == 0 {
+					continue
+				}
+				result = append(result, out)
+			}
+		case []ref.Val:
+			for _, v := range val.Value().([]ref.Val) {
+				out, ok := v.Value().(string)
+				if !ok {
+					return nil, fmt.Errorf("expression must return a string or a list of strings")
+				}
+				if len(out) == 0 {
+					continue
+				}
+				result = append(result, out)
+			}
+		default:
+			return nil, fmt.Errorf("expression must return a string or a list of strings")
+		}
+
+		if len(result) == 0 {
+			return nil, nil
+		}
+
+		return result, nil
+	case celgo.NullType.TypeName():
+		return nil, nil
+	default:
+		return nil, fmt.Errorf("expression must return a string or a list of strings")
+	}
 }
